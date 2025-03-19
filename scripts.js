14c7dd4e3a4bbd5682bc3a2d8e93117741a6942c
Nolantrob <nolanrobertson97@gmail.com>
2025-03-18 20:13:16 -0400

    Add functionality for symbols, partial keyboard support, and refactor how expression is stored

--- scripts.js
+++ scripts.js
@@ -1,21 +1,63 @@
-const buttonContainer = document.querySelector('#button-container');
-const numberButtons = buttonContainer.querySelectorAll('.number-button');
 const display = document.querySelector('#display');
+const buttonContainer = document.querySelector('#button-container');
+
 const clearDisplayButton = buttonContainer.querySelector('.clear-display-button');
+const numberButtons = buttonContainer.querySelectorAll('.number-button');
+const backspaceButton = buttonContainer.querySelector('.backspace-button');
+const operatorButtons = buttonContainer.querySelectorAll('.operator-button');
+const equalsButton = buttonContainer.querySelector('.equals-button');
+
+let leftArgument = 0;
+let rightArgument = '';
+let currentOperator = '';
+let expression = `${leftArgument}${currentOperator}${rightArgument}`;
+updateDisplay();
+
+// equalsButton.addEventListener('click', () => {
+//     operate()
+// })
+
+document.addEventListener('keypress', (event) => {
+    if (event.key <= 10){
+        addCharacterToExpression(event.key);
+    }
+})
+
+function addCharacterToExpression(char) {
+    if (expression.length >= 15){
+        return;
+    }
+
+    expression === '0' ? expression = char : expression += char;
+    updateDisplay();    
+}
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        if (!expression.includes(button.textContent)){
+            expression += `${button.textContent}`;
+            updateDisplay();
+        }
+    })
+})
 
 clearDisplayButton.addEventListener('click', () => {
     clearDisplay();
 })
+backspaceButton.addEventListener('click', () => {
+    backspace();
+})
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        display.textContent += button.textContent;
+        addCharacterToExpression(button.textContent)
     })
 })
 
-let leftNum;
-let rightNum;
-let currentOperator;
+function updateDisplay() {
+    display.textContent = expression;
+}
 
 add = function(num1,num2) {
     return num1 + num2;
@@ -31,9 +73,6 @@ multiply = function(num1, num2) {
 
 divide = function(num1,num2) {
     return num1 / num2;
-}
-
-togglePositiveNegative = function() {
 }
 
 operate = function(operator,num1,num2) {
@@ -56,5 +95,11 @@ operate = function(operator,num1,num2) {
 }
 
 function clearDisplay() {
-    display.textContent = '';
+    expression = '0';
+    updateDisplay();
+}
+
+function backspace() {
+    expression = expression.slice(0,-1);
+    updateDisplay();
 }
